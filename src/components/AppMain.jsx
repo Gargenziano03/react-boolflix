db9b9b23c396d5a7453516c067373b47ce99f9a6
Gargenziano03 <gargenziano03@gmail.com>
2024-11-29 15:19:03 +0100

    add languageFlags

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,9 +1,12 @@
 import { useGlobalContext } from "../GlobalContext/GlobalContext"
-
+import Flag from "react-world-flags"
 
 export default function AppMain() {
     //destrutturo movies dal GlobalContext
     const { movies } = useGlobalContext()
+    const languageFlags = {
+        en: 'gb'
+    }
 
     return (
         <div className="container">
@@ -16,7 +19,7 @@ export default function AppMain() {
                                 {movie.title} <br />
                                 {movie.vote_average} <br />
                                 {movie.original_language}
-
+                                <Flag code={languageFlags[movie.original_language]} style={{ height: 15 }} />
                             </li>
                         ))}
 
